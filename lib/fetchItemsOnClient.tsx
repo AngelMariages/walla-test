fd604e1f6a8222b4d6409295467636c1d6c963fa
Àngel Mariages <angel.mariages@gmail.com>
2022-11-29 22:39:50 +0100

    feat(api): abort request when new one comes

--- lib/fetchItemsOnClient.tsx
+++ lib/fetchItemsOnClient.tsx
@@ -1,11 +1,18 @@
 import { Sort } from 'constants/sortBy';
 import { ApiResult } from 'pages/api/items';
 
+const abortControllers: AbortController[] = [];
+
 export const fetchItemsOnClient = async (
     page = 1,
     search?: string,
     sort?: Sort
 ): Promise<ApiResult> => {
+    for (let i = 0; i < abortControllers.length; i++) {
+        abortControllers.shift()?.abort();
+    }
+
+    const abortController = new AbortController();
     const params = new URLSearchParams();
 
     params.append('page', page.toString());
@@ -17,7 +24,9 @@ export const fetchItemsOnClient = async (
         params.append('sortOrder', sort.sortOrder);
     }
 
-    return await fetch(`/api/items?${params.toString()}`).then((res) =>
-        res.json()
-    );
+    abortControllers.push(abortController);
+
+    return await fetch(`/api/items?${params.toString()}`, {
+        signal: abortController.signal,
+    }).then((res) => res.json());
 };
